bb32c7533e7f55d02558128876efa4a89804b5f7
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-08-16 09:57:20 -0700

    chore: re-export touchscreen from mod.ts

--- mod.ts
+++ mod.ts
@@ -4,4 +4,5 @@ export * from "./src/elementHandle.ts";
 export * from "./src/keyboard.ts";
 export * from "./src/mouse.ts";
 export * from "./src/page.ts";
+export * from "./src/touchscreen.ts";
 export * from "./src/util.ts";
