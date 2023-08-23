9de13b43a11702946724f71bec68554da1157157
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-08-23 10:19:24 -0700

    feat: ElementHandle.innerText/HTML

--- tests/evaluate_test.ts
+++ tests/evaluate_test.ts
@@ -1,8 +1,9 @@
 import { assertEquals } from "https://deno.land/std@0.198.0/assert/assert_equals.ts";
+import { assertSnapshot } from "https://deno.land/std@0.198.0/testing/snapshot.ts";
 
 import { launch } from "../mod.ts";
 
-Deno.test("Testing evaluate", async () => {
+Deno.test("Testing evaluate", async (t) => {
   // Launch browser
   const browser = await launch();
 
@@ -16,6 +17,11 @@ Deno.test("Testing evaluate", async () => {
     args: ["string", "concat"] as const,
   });
   assertEquals(result, "stringconcat");
+
+  // innerHTML / innerText
+  const element = (await page.$("div"))!;
+  assertSnapshot(t, await element.innerHTML());
+  assertSnapshot(t, await element.innerText());
 
   // Close browser
   await browser.close();
