676a04f04f69fe8a0b819b336146ca7a584c2645
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2023-10-09 16:29:24 +0000

    feat: add `page.setContent`

---
+++ tests/set_content_test.ts
@@ -0,0 +1,32 @@
+import { assertStrictEquals } from "https://deno.land/std@0.201.0/assert/assert_strict_equals.ts";
+import { launch } from "../mod.ts";
+
+Deno.test("Set content", async () => {
+  // Launch browser
+  const browser = await launch();
+  const content = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Astral</title>
+    </head>
+    <body>
+      <span>Hello world</span>
+    </body>
+  </html>`;
+
+  // Open the webpage and set content
+  const page = await browser.newPage();
+  await page.setContent(content);
+
+  // Wait for selector
+  assertStrictEquals(
+    content.replace(/\s/g, ""),
+    `${await page.content()}`.replace(/\s/g, ""),
+  );
+  const selected = await page.waitForSelector("span");
+  assertStrictEquals(await selected.innerText(), "Hello world");
+
+  // Close browser
+  await browser.close();
+});
