7baf0d4e11249e4d114b1601f71f7358e472496c
Simon Lecoq <22963968+lowlighter@users.noreply.github.com>
2023-10-13 02:48:44 +0000

    feat: support shared browser install for concurrent getBinary() calls

--- src/cache.ts
+++ src/cache.ts
@@ -5,7 +5,8 @@ import { dirname } from "https://deno.land/std@0.201.0/path/dirname.ts";
 import { join } from "https://deno.land/std@0.201.0/path/join.ts";
 import { ZipReader } from "https://deno.land/x/zipjs@v2.7.29/index.js";
 import ProgressBar from "https://deno.land/x/progress@v1.3.9/mod.ts";
-import { exists } from "https://deno.land/std@0.203.0/fs/exists.ts";
+import { exists, existsSync } from "https://deno.land/std@0.203.0/fs/exists.ts";
+import { retry } from "https://deno.land/std@0.203.0/async/retry.ts";
 
 export const SUPPORTED_VERSIONS = {
   chrome: "118.0.5943.0",
@@ -17,6 +18,8 @@ const HOME_PATH = Deno.build.os === "windows"
   : Deno.env.get("HOME")!;
 const BASE_PATH = resolve(HOME_PATH, ".astral");
 const CONFIG_FILE = "cache.json";
+
+const LOCK_FILES = {} as { [cache: string]: { [product: string]: Lock } };
 
 interface KnownGoodVersions {
   timestamps: string;
@@ -53,6 +56,7 @@ function getCachedConfig({ cache = BASE_PATH }): Record<string, string> {
 export async function cleanCache({ cache = BASE_PATH } = {}) {
   try {
     if (await exists(cache)) {
+      delete LOCK_FILES[cache];
       await Deno.remove(cache, { recursive: true });
     }
   } catch (error) {
@@ -120,19 +124,42 @@ async function decompressArchive(source: string, destination: string) {
 /**
  * Get path for the binary for this OS. Downloads a browser if none is cached.
  */
-export async function getBinary(
+export function getBinary(
   browser: "chrome" | "firefox",
   { cache = BASE_PATH } = {},
 ): Promise<string> {
+  const product = `${browser}-${SUPPORTED_VERSIONS[browser]}`;
+  return _getBinary(browser, { cache }).finally(() =>
+    LOCK_FILES[cache]?.[product]?.release()
+  );
+}
+
+export async function _getBinary(
+  browser: Parameters<typeof getBinary>[0],
+  { cache = BASE_PATH }: NonNullable<Parameters<typeof getBinary>[1]>,
+): Promise<string> {
   // TODO(lino-levan): fix firefox downloading
   const VERSION = SUPPORTED_VERSIONS[browser];
+  const product = `${browser}-${SUPPORTED_VERSIONS[browser]}`;
 
   const config = getCachedConfig({ cache });
+
+  // If the config doesn't have the revision and there is a lock file, reload config after release
+  if (!config[VERSION] && LOCK_FILES[cache]?.[product]?.exists()) {
+    await LOCK_FILES[cache]?.[product]?.waitRelease();
+    Object.assign(config, getCachedConfig({ cache }));
+  }
 
   // If the config doesn't have the revision, download it and return that
   if (!config[VERSION]) {
     const quiet = await isQuietInstall();
     ensureDirSync(cache);
+    const lock = new Lock({ cache });
+    LOCK_FILES[cache] ??= {};
+    LOCK_FILES[cache][product] = lock;
+    if (!lock.create()) {
+      return _getBinary(browser, { cache });
+    }
     const versions = await knownGoodVersions();
     const version = versions.versions.filter((val) =>
       val.version === VERSION
@@ -224,4 +251,64 @@ export async function getBinary(
   throw new Error(
     "Unsupported platform, provide a path to a chromium or firefox binary instead",
   );
+}
+
+/**
+ * Create a lock file in cache
+ * Only the process with the same PID can release created lock file through this API
+ * TODO: Use Deno.flock/Deno.funlock when stabilized (https://deno.land/api@v1.37.1?s=Deno.flock&unstable)
+ */
+class Lock {
+  readonly path;
+
+  constructor({ cache = BASE_PATH } = {}) {
+    this.path = resolve(cache, ".lock");
+  }
+
+  /** Returns true if lock file exists */
+  exists() {
+    return existsSync(this.path);
+  }
+
+  /** Create a lock file and returns true if it succeeds, false if it was already existing */
+  create() {
+    try {
+      Deno.writeTextFileSync(this.path, `${Deno.pid}`, { createNew: true });
+      return true;
+    } catch (error) {
+      if (!(error instanceof Deno.errors.AlreadyExists)) {
+        throw error;
+      }
+      return false;
+    }
+  }
+
+  /** Release lock file */
+  release() {
+    try {
+      if (Deno.readTextFileSync(this.path) === `${Deno.pid}`) {
+        Deno.removeSync(this.path);
+      }
+    } catch (error) {
+      if (!(error instanceof Deno.errors.NotFound)) {
+        throw error;
+      }
+    }
+  }
+
+  /** Wait for lock release */
+  async waitRelease({ timeout = 60000 } = {}) {
+    await retry(() => {
+      if (this.exists()) {
+        throw new Error(
+          `Timeout while waiting for lockfile release at: ${this.path}`,
+        );
+      }
+    }, {
+      maxTimeout: timeout,
+      maxAttempts: Infinity,
+      multiplier: 1,
+      minTimeout: 100,
+    });
+  }
 }
