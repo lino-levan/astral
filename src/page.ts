25ba3698981754fcb7b6354b3a5f10a5b03b18a9
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-06-06 07:44:30 -0700

    chore: move content to use the built-in evaluate

--- src/page.ts
+++ src/page.ts
@@ -338,15 +338,7 @@ export class Page extends EventTarget {
    */
   async content(): Promise<string> {
     // https://stackoverflow.com/questions/6088972/get-doctype-of-an-html-as-string-with-javascript
-    const { result } = await retryDeadline(
-      this.#celestial.Runtime.evaluate({
-        expression:
-          `"<!DOCTYPE " + document.doctype.name + (document.doctype.publicId ? ' PUBLIC "' + document.doctype.publicId + '"' : '') + (!document.doctype.publicId && document.doctype.systemId ? ' SYSTEM' : '') + (document.doctype.systemId ? ' "' + document.doctype.systemId + '"' : '') + '>\\n' + document.documentElement.outerHTML`,
-      }),
-      this.timeout,
-    );
-
-    return result.value;
+    return await this.evaluate(`"<!DOCTYPE " + document.doctype.name + (document.doctype.publicId ? ' PUBLIC "' + document.doctype.publicId + '"' : '') + (!document.doctype.publicId && document.doctype.systemId ? ' SYSTEM' : '') + (document.doctype.systemId ? ' "' + document.doctype.systemId + '"' : '') + '>\\n' + document.documentElement.outerHTML`);
   }
 
   /**
