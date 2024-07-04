688ccbfd393787f4a0cc90d55216a39f5f33756c
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-07-04 08:56:36 -0700

    chore: downgrade

--- src/cache.ts
+++ src/cache.ts
@@ -8,7 +8,7 @@ import { ZipReader } from "@zip-js/zip-js";
 import ProgressBar from "@deno-library/progress";
 
 export const SUPPORTED_VERSIONS = {
-  chrome: "126.0.6478.126",
+  chrome: "124.0.6367.207",
   firefox: "116.0",
 } as const;
 
