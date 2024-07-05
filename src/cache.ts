db112f6530d8136de0880ed9855d3890fa60dfe3
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-07-05 09:01:24 -0700

    chore: bump version

--- src/cache.ts
+++ src/cache.ts
@@ -8,7 +8,7 @@ import { ZipReader } from "@zip-js/zip-js";
 import ProgressBar from "@deno-library/progress";
 
 export const SUPPORTED_VERSIONS = {
-  chrome: "125.0.6400.0",
+  chrome: "125.0.6415.0",
   firefox: "116.0",
 } as const;
 
