2d0ea76e526227705b583e02b903a4736a42f16a
Jim Montgomery <github@jimmont.com>
2024-11-12 10:44:32 +0800

    add working test scenario example showing use of path and other options to fix https://github.com/lino-levan/astral/issues/107

--- README.md
+++ README.md
@@ -58,6 +58,31 @@ console.log(result);
 await browser.close();
 ```
 
+```js
+import { launch } from "jsr:@astral/astral";
+import { assert } from "jsr:@std/assert";
+
+const browser = await launch({
+  // macOS specific path option
+  path: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+  args: ['--remote-debugging-port=9222', '--headless=new', '--no-first-run', '--password-store=basic', '--use-mock-keychain', '--hide-scrollbars', '--no-sandbox']
+});
+
+const page = await browser.newPage("https://deno.com");
+
+// Run code in the context of the browser
+const text = await page.evaluate(() => {
+	return document.body.querySelector('main h1')?.textContent ?? '';
+});
+
+Deno.test('test page content', async ()=>{
+	assert(text.includes('JavaScript'), 'has JavaScript');
+});
+
+await browser.close();
+
+```
+
 You can navigate to a page and interact with it.
 
 ```ts
---
+++ examples/page-content-test.js
@@ -0,0 +1,25 @@
+/* basic working example for a UI test with macOS specific path
+ * deno test -A ./page-content-test.js
+ * see also https://docs.deno.com/runtime/fundamentals/testing/
+ * */
+import { launch } from "jsr:@astral/astral";
+import { assert } from "jsr:@std/assert";
+
+const browser = await launch({
+  // macOS specific path option
+  path: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+  args: ['--remote-debugging-port=9222', '--headless=new', '--no-first-run', '--password-store=basic', '--use-mock-keychain', '--hide-scrollbars', '--no-sandbox']
+});
+
+const page = await browser.newPage("https://deno.com");
+
+// Run code in the context of the browser
+const text = await page.evaluate(() => {
+  return document.body.querySelector('main h1')?.textContent ?? '';
+});
+
+Deno.test('test page content', async ()=>{
+  assert(text.includes('JavaScript'), 'has JavaScript');
+});
+
+await browser.close();
