5ed4bcc2e834036cf0bb9a550a1ad2c5f2ce32ea
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2025-01-20 09:32:02 -0800

    fix: astral in deploy crashing due to querySync

--- src/debug.ts
+++ src/debug.ts
@@ -1,7 +1,35 @@
+// Needed to fix `Deno.permissions.querySync` not being defined in Deno Deploy
+// See: https://github.com/denoland/deploy_feedback/issues/527
+let querySync = Deno.permissions.querySync;
+if (!querySync) {
+  const permissions = {
+    run: "denied",
+    read: "granted",
+    write: "denied",
+    net: "granted",
+    env: "granted",
+    sys: "denied",
+    ffi: "denied",
+  } as const;
+
+  querySync = ({ name }) => {
+    return {
+      state: permissions[name],
+      onchange: null,
+      partial: false,
+      addEventListener() {},
+      removeEventListener() {},
+      dispatchEvent() {
+        return false;
+      },
+    };
+  };
+}
+
 /** Whether to enable debug logging. */
 export const DEBUG =
-  (Deno.permissions.querySync({ name: "env", variable: "DEBUG" }).state ===
-    "granted") && (!!Deno.env.get("DEBUG"));
+  (querySync({ name: "env", variable: "DEBUG" }).state === "granted") &&
+  (!!Deno.env.get("DEBUG"));
 
 /** Attach a websocket to the console for debugging. */
 export function attachWs(ws: WebSocket) {
