1085b2668116f6010833fdf3faf6fc3e35545362
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2025-04-30 17:38:43 -0700

    chore: clean up locator implementation (#147)
    * chore: clean up locator implementation
    
    * fmt

--- src/locator.ts
+++ src/locator.ts
@@ -19,63 +19,53 @@ export class Locator<T> {
     this.#timeout = timeout;
   }
 
-  /** Clicks the element. */
-  async click() {
-    await retry(async () => {
-      const p = this.#click();
+  async #runLocator<T>(func: (handle: ElementHandle) => Promise<T>) {
+    return await retry(async () => {
+      const p = (async () => {
+        await this.#page.waitForSelector(this.#selector);
+        const handle = await this.#page.$(this.#selector);
+        if (handle === null) {
+          throw new Error(`Selector "${this.#selector}" not found.`);
+        }
+
+        return await func(handle);
+      })();
       await deadline(p, this.#timeout);
+      return await p;
     });
   }
 
-  async #click() {
-    await this.#page.waitForSelector(this.#selector);
-    const handle = await this.#page.$(this.#selector);
-    if (handle === null) {
-      throw new Error(`Selector "${this.#selector}" not found.`);
-    }
+  /** Clicks the element. */
+  async click() {
+    await this.#runLocator(async (handle) => {
+      await handle.click();
+    });
+  }
 
-    await handle.click();
+  /** Evaluates the given function in the context of the element. */
+  async evaluate<R>(fn: (el: T) => R): Promise<R> {
+    return await this.#runLocator(async (handle) => {
+      // deno-lint-ignore no-explicit-any
+      return await handle.evaluate(fn, { args: [handle] as any }) as R;
+    });
   }
 
   /** Fills the element with the given text. */
   async fill(text: string) {
-    await retry(async () => {
-      const p = this.#fill(text);
-      await deadline(p, this.#timeout);
+    await this.#runLocator(async (handle) => {
+      await handle.type(text);
     });
   }
 
-  async #fill(text: string) {
-    await this.#page.waitForSelector(this.#selector);
-    const handle = await this.#page.$(this.#selector);
-    if (handle === null) {
-      throw new Error(`Selector "${this.#selector}" not found.`);
-    }
-
-    await handle.type(text);
+  /** Focuses the element. */
+  async focus() {
+    await this.#runLocator(async (handle) => {
+      await handle.focus();
+    });
   }
 
   /** Waits for the element to appear in the page. */
   async wait(): Promise<ElementHandle> {
     return await this.#page.waitForSelector(this.#selector);
-  }
-
-  /** Evaluates the given function in the context of the element. */
-  async evaluate<R>(fn: (el: T) => R): Promise<R> {
-    return await retry(async () => {
-      const p = this.#evaluate(fn);
-      return await deadline(p, this.#timeout);
-    });
-  }
-
-  async #evaluate<R>(fn: (el: T) => R): Promise<R> {
-    await this.#page.waitForSelector(this.#selector);
-    const handle = await this.#page.$(this.#selector);
-    if (handle === null) {
-      throw new Error(`Selector "${this.#selector}" not found.`);
-    }
-
-    // deno-lint-ignore no-explicit-any
-    return await handle.evaluate(fn, { args: [handle] as any }) as R;
   }
 }
